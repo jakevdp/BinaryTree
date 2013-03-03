c833438f89f853295564d3843b612ffb5711a906
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-03-03 09:03:46 -0800

    add scipy KDTree to  bench_neighbors script

--- bench_neighbors.py
+++ bench_neighbors.py
@@ -1,8 +1,11 @@
+import warnings
 from time import time
 import numpy as np
+from scipy.spatial import cKDTree
 from sklearn.neighbors import BallTree as skBallTree
 from ball_tree import BallTree
 from kd_tree import KDTree
+
 
 def time_func(executable, *args, **kwargs):
     N = 5
@@ -18,10 +21,16 @@ def time_func(executable, *args, **kwargs):
 def bench_trees(N=2000, D=3, leaf_size=40, metric='euclidean', **kwargs):
     print "{N} points, metric = {metric}".format(N=N, metric=metric)
 
+    if metric not in ('euclidean', 'minkowski'):
+        warnings.warn("sklearn BallTree & scipy KDTree "
+                      "cannot handle metric='%s': "
+                      "results will not be comparable'")
+    p = kwargs.get('p', 2)
+
     X = np.random.random((N, D))
 
-    labels = ['skBallTree', 'BallTree', 'KDTree']
-    tree_types = [skBallTree, BallTree, KDTree]
+    labels = ['cKDTree', 'skBallTree', 'BallTree', 'KDTree']
+    tree_types = [cKDTree, skBallTree, BallTree, KDTree]
 
     spacing = 11
 
@@ -29,18 +38,31 @@ def bench_trees(N=2000, D=3, leaf_size=40, metric='euclidean', **kwargs):
                                           ('', 'Build',
                                            'KNN', 'RNN', 'KDE', '2PT')))
     for tree_type, label in zip(tree_types, labels):
-        if label == 'skBallTree':
-            tree = tree_type(X, leaf_size=leaf_size)
-            construct_time = time_func(tree_type, X, leaf_size=leaf_size)
+        if label == 'cKDTree':
+            tree = tree_type(X, leafsize=leaf_size)
+            construct_time = time_func(tree_type, X, leafsize=leaf_size)
+            knn_time = time_func(tree.query, X, k=3, p=p)
+            print '%s %s %s' % tuple(map(lambda s: s.ljust(spacing),
+                                         (label,
+                                          '%.2g' % construct_time,
+                                          '%.2g' % knn_time)))
+        elif label == 'skBallTree':
+            tree = tree_type(X, leaf_size=leaf_size, p=p)
+            construct_time = time_func(tree_type, X, leaf_size=leaf_size, p=p)
+            knn_time = time_func(tree.query, X, k=3)
+            rnn_time = time_func(tree.query_radius, X, r=0.1)
+            print '%s %s %s %s' % tuple(map(lambda s: s.ljust(spacing),
+                                            (label,
+                                             '%.2g' % construct_time,
+                                             '%.2g' % knn_time,
+                                             '%.2g' % rnn_time)))
         else:
             tree = tree_type(X, leaf_size=leaf_size,
                              metric=metric, **kwargs)
             construct_time = time_func(tree_type, X, leaf_size=leaf_size,
                                        metric=metric, **kwargs)
-        knn_time = time_func(tree.query, X, k=3)
-        rnn_time = time_func(tree.query_radius, X, r=0.1)
-
-        if not label.startswith('sk'):
+            knn_time = time_func(tree.query, X, k=3)
+            rnn_time = time_func(tree.query_radius, X, r=0.1)
             kde_time = time_func(tree.kernel_density, X,
                                  h=0.01, rtol=0.001, atol=0.001)
             twopt_time = time_func(tree.two_point_correlation, X,
@@ -52,15 +74,9 @@ def bench_trees(N=2000, D=3, leaf_size=40, metric='euclidean', **kwargs):
                                                    '%.2g' % rnn_time,
                                                    '%.2g' % kde_time,
                                                    '%.2g' % twopt_time)))
-        else:
-            print '%s %s %s %s' % tuple(map(lambda s: s.ljust(spacing),
-                                            (label,
-                                             '%.2g' % construct_time,
-                                             '%.2g' % knn_time,
-                                             '%.2g' % rnn_time)))
     print
 
 
 if __name__ == '__main__':
-    bench_trees(1000, metric='euclidean')
-    bench_trees(1000, metric='minkowski', p=3)
+    bench_trees(2000, metric='euclidean')
+    bench_trees(2000, metric='minkowski', p=1)
